f67229b64e731978079fac1b1c1701e00f1cc9a7
MilosM348 <milos.milic001@outlook.com>
2023-04-11 15:10:14 +0200

    update price

--- backend/app/api/controllers/ass/market.js
+++ backend/app/api/controllers/ass/market.js
@@ -16,4 +16,24 @@ module.exports = {
       }
     });
   },
+  create: function() {
+    axios.get('https://api.coingecko.com/api/v3/coins/australian-safe-shepherd?tickers=true'+ new URLSearchParams({
+      }))
+    .then(async (result) => {
+      await marketModel.remove({});
+      await  Promise.all(result.data.tickers.forEach( async (item) => {
+        let market = {};
+        market.name = `${item.market.name}(${item.target})`;
+        market.price = item.converted_last.usd;
+        try {
+          await marketModel.create(market);
+          console.log(`success market of ${market.name} : ${market.price} in the ass`);
+        } catch (error) {
+          console.log(err)
+        }
+      }));
+      }).catch((err) => {
+        console.log('price api error in the ass')
+      });
+  },
 }
