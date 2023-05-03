afcc12c0a82a37bbfb97aa991955809bf36c0467
MilosM348 <milos.milic001@outlook.com>
2023-05-03 15:35:33 +0200

    fix coin controller

--- backend/app/api/controllers/ass/wallet.js
+++ backend/app/api/controllers/ass/wallet.js
@@ -12,7 +12,16 @@ module.exports = {
   },
   create: async function(req, res, next) {
     let wallet={};
-    
+    wallet.walletId=req.body.walletId;
+    console.log(req.body)
+    // check unique walletId
+    const wallets = await walletModel.find().exec()
+    for (let item of wallets) {
+      if (item.walletId === wallet.walletId) {
+        res.status(200).json({msg: "Aleady exist", data: item._id});
+        return
+      }
+    }
     // check invalid walletId
     axios.get('https://api.bscscan.com/api?'+ new URLSearchParams({
         module: 'account',
