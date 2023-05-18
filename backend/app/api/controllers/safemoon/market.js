e70c86d1b2f66e30f15b88b77c644ac7abad9ca7
MilosM348 <milos.milic001@outlook.com>
2023-05-18 17:46:08 +0200

    fix safemoon controller

--- backend/app/api/controllers/safemoon/market.js
+++ backend/app/api/controllers/safemoon/market.js
@@ -2,15 +2,7 @@ const axios = require('axios');
 const marketModel = require('../../models/safemoon/market');	
 
 module.exports = {
-  getMarket: async function(req, res, next) {
-    marketModel.find({}, function(err, result){
-      if (err){
-        res.status(400).json({ msg: "Not found" });
-      } else{				
-        res.status(200).json({msg: "Result found!", data: result});							
-      }
-    });
-  },
+  
   create: function() {
     axios.get('https://api.coingecko.com/api/v3/coins/safemoon?tickers=true'+ new URLSearchParams({
       }))
