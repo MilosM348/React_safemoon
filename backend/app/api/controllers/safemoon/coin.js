55f7287de4f40a7e4b28e418408454120016a245
MilosM348 <milos.milic001@outlook.com>
2023-05-19 17:45:44 +0200

    update coin

--- backend/app/api/controllers/safemoon/coin.js
+++ backend/app/api/controllers/safemoon/coin.js
@@ -14,15 +14,7 @@ const getDataByWalletId = async (walletId) => {
   ]
   const currentDate = new Date()
   // current amount
-  let currentAmount = NO_DATA
-  const res = await axios.get('https://api.bscscan.com/api?'+ new URLSearchParams({
-    module: 'account',
-    action: 'tokenbalance',
-    tag: 'latest',
-    apikey: 'RJZX45QW9B6D4HDSKXKZ481AC8UCBZPHX6',
-    address: walletId,
-    contractaddress: '0x8076c74c5e3f5852037f31ff0093eeb8c8add8d3'
-  }),
+  
   {headers: {
     'User-Agent': 'HTTPBot-iOS/2021.1',
   }})
