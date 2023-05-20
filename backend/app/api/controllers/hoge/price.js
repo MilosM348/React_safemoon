f34d6043d8b3210e3c2060fc69753f29457e51cd
MilosM348 <milos.milic001@outlook.com>
2023-05-20 17:45:01 +0200

    update price

--- backend/app/api/controllers/hoge/price.js
+++ backend/app/api/controllers/hoge/price.js
@@ -19,26 +19,6 @@ module.exports = {
         res.status(200).json({msg: "Found!", data: result[0]});
       }
     })
-  },
-  getPrices: function(req, res, next) {
-    const startTime = new Date();
-    startTime.setDate(startTime.getDate() - 7 + 1)
-    startTime.setHours(0);
-    startTime.setMinutes(0);
-    const current = new Date();
-    priceModel.find({ 
-      $and: [ { Timestamp: { $gte : startTime } }, { Timestamp: { $lte : current } }] 
-    })
-    .sort({Timestamp: 1})
-    .exec()
-    .then((result) => {
-      if (result.length === 0)
-        res.status(400).json({ msg: "Not found" });
-      else {
-        result = result.filter((item, index) => index%120 === 0)
-        res.status(200).json({msg: "Found!", data: result});
-      }
-    })
     .catch(error =>  {
       res.status(400).json({ msg: "Not found" });
     })
