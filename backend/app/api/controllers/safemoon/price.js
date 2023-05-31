1c23f4ecab4d0d73d12d1f7487547f5a52054120
MilosM348 <milos.milic001@outlook.com>
2023-05-31 08:19:43 +0200

    fix wallet

--- backend/app/api/controllers/safemoon/price.js
+++ backend/app/api/controllers/safemoon/price.js
@@ -19,6 +19,9 @@ module.exports = {
         res.status(200).json({msg: "Found!", data: result[0]});
       }
     })
+    .catch(error =>  {
+      res.status(400).json({ msg: "Not found" });
+    })
   },
   getPrices: function(req, res, next) {
     const startTime = new Date();
