65fffbe6a4dbe8caee8d709c713a98051df7a448
MilosM348 <milos.milic001@outlook.com>
2023-06-30 16:16:08 +0200

    fix

--- backend/app/api/controllers/safemoon/price.js
+++ backend/app/api/controllers/safemoon/price.js
@@ -10,6 +10,18 @@ module.exports = {
     priceModel.find({ 
       $and: [ { Timestamp: { $gte : startTime } }, { Timestamp: { $lte : current } }] 
     })
+    .sort({Timestamp: -1})
+    .exec()
+    .then((result) => {
+      if (result.length === 0)
+        res.status(400).json({ msg: "Not found" });
+      else {
+        res.status(200).json({msg: "Found!", data: result[0]});
+      }
+    })
+    .catch(error =>  {
+      res.status(400).json({ msg: "Not found" });
+    })
   },
   getPrices: function(req, res, next) {
     const startTime = new Date();
